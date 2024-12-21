1414482de1ed1aadeaa154c0080d49d834b3407c
himanishpuri <himanishpuri2203@gmail.com>
2024-12-21 21:59:12 +0530

    minor

--- src/Pages/LandingPage/Component/Navbar/NewNavbar.tsx
+++ src/Pages/LandingPage/Component/Navbar/NewNavbar.tsx
@@ -9,8 +9,10 @@ import NavButton from "./Assets/NavButton";
 import ccLogo from "./Assets/ccs_20.png";
 import menuLogo from "./Assets/menuLogo.svg";
 import { ImgHTMLAttributes } from "react";
+import { useNavigate } from "react-router";
 
 export default function NewNavbar() {
+	const navigate = useNavigate();
 	return (
 		<header className="fixed flex h-16 mt-5 w-[90dvw] mx-[5dvw] rounded-md shrink-0 items-center px-4 md:px-6 bg-[#ffffff1a] text-white">
 			<Sheet>
@@ -19,6 +21,9 @@ export default function NewNavbar() {
 						variant="outline"
 						size="icon"
 						className="lg:hidden bg-transparent p-2"
+						onClick={() => {
+							navigate("/");
+						}}
 					>
 						<img
 							src={menuLogo}
@@ -27,37 +32,47 @@ export default function NewNavbar() {
 						<span className="sr-only">Toggle navigation menu</span>
 					</Button>
 				</SheetTrigger>
-				<SheetContent side="left">
+				<SheetContent
+					className="bg-transparent text-white border-none"
+					side="left"
+				>
 					<button className="mr-6 lg:flex">
 						<CCSLogo />
-						<span className="sr-only">Acme Inc</span>
+						<span className="sr-only">HackTU</span>
 					</button>
 					<div className="grid gap-2 py-6">
-						<button className="flex w-full items-center py-2 text-lg font-semibold">
+						<button className="flex w-full items-center p-2 text-lg font-semibold hover:bg-[#192C4C] rounded-md duration-300">
 							ABOUT
 						</button>
-						<button className="flex w-full items-center py-2 text-lg font-semibold">
+						<button className="flex w-full items-center p-2 text-lg font-semibold hover:bg-[#192C4C] rounded-md duration-300">
 							SCHEDULE
 						</button>
-						<button className="flex w-full items-center py-2 text-lg font-semibold">
+						<button className="flex w-full items-center p-2 text-lg font-semibold hover:bg-[#192C4C] rounded-md duration-300">
 							SPONSORS
 						</button>
-						<button className="flex w-full items-center py-2 text-lg font-semibold">
+						<button className="flex w-full items-center p-2 text-lg font-semibold hover:bg-[#192C4C] rounded-md duration-300">
 							FAQs
 						</button>
-						<button className="flex w-full items-center py-2 text-lg font-semibold">
+						<button className="flex w-full items-center p-2 text-lg font-semibold hover:bg-[#192C4C] rounded-md duration-300">
 							TEAM
 						</button>
-						<button className="flex w-full items-center py-2 text-lg font-semibold">
-							Register
+						<button className="flex w-full items-center p-2 text-lg font-semibold hover:bg-[#192C4C] rounded-md duration-300">
+							REGISTER
 						</button>
 					</div>
 				</SheetContent>
 			</Sheet>
-			<button className="mr-6 hidden lg:flex">
+			<Button
+				variant="outline"
+				size="icon"
+				className="mr-6 hidden lg:flex"
+				onClick={() => {
+					navigate("/");
+				}}
+			>
 				<CCSLogo />
 				<span className="ml-3">HackTU</span>
-			</button>
+			</Button>
 			<nav className="ml-auto hidden lg:flex gap-6">
 				<NavButton text="ABOUT" />
 				<NavButton text="SCHEDULE" />
