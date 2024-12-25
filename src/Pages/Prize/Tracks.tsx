836f31c5434a186d6fb9de94c8b8a6afdcb3d5ea
Aayush <agoyal1_be23@thapar.edu>
2024-12-26 02:00:14 +0530

    tracks

--- src/Pages/Prize/Tracks.tsx
+++ src/Pages/Prize/Tracks.tsx
@@ -1,25 +1,14 @@
-import React from 'react'
+import HeaderText from "@/components/ui/HeaderText";
+import React from "react";
+import track from "./Assets/tracks.png";
 
 const Tracks = () => {
   return (
-    <>
-    <h1 className='justify-center flex text-4xl font-bold p-9 text-white bg-green-700'>Hackathon Tracks</h1>
-      <div className='h-screen w-full bg-zinc-600 flex flex-col justify-center align-center px-8 '>
-        <div className='w-full bg-pink-700 h-[30vh] flex gap-5 justify-center align-center '>
-          <div className='bg-yellow-500 h-[29vh] w-[25vw] rounded-lg'></div>
-          <div className='bg-yellow-500 h-[29vh] w-[25vw] rounded-lg'></div>
-          <div className='bg-yellow-500 h-[29vh] w-[25vw] rounded-lg'></div>
-          <div className='bg-yellow-500 h-[29vh] w-[25vw] rounded-lg'></div>
-        </div>
-        <div className='w-full bg-pink-700 h-[30vh] mt-4 flex gap-4 justify-center align-center'>
-          <div className='bg-yellow-500 h-[29vh] w-[25vw] rounded-lg'></div>
-          <div className='bg-yellow-500 h-[29vh] w-[25vw] rounded-lg'></div>
-          <div className='bg-yellow-500 h-[29vh] w-[25vw] rounded-lg'></div>
+    <div className="bg-[#0F203E] justify-center items-center w-full flex flex-col py-5">
+      <HeaderText text="TRACKS" />
+      <img src={track} className=" w-[75%] bg-[#0F203E] h-full" alt="" />
+    </div>
+  );
+};
 
-        </div>
-      </div>
-    </>
-  )
-}
-
-export default Tracks
+export default Tracks;
