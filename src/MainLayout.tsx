fa7398b89304cb7651cb3956477c1998592b895b
Aayush <agoyal1_be23@thapar.edu>
2024-12-25 23:51:31 +0530

    made the header text component

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -2,16 +2,18 @@ import NewNavbar from "./Pages/LandingPage/Component/Navbar/NewNavbar";
 import Landing from "./Pages/LandingPage/Landing";
 import WhyParticipate from "./Pages/WhyParticipate/WhyParticipate";
 import Footer from "./Pages/Footer/Footer";
+import HeaderText from "./components/ui/HeaderText";
 
 function MainLayout() {
-	return (
-		<>
-			<NewNavbar />
-			<Landing />
-			<WhyParticipate />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <NewNavbar />
+      <Landing />
+      <HeaderText text="ABOUT HACKTU 6.0" />
+      <WhyParticipate />
+      <Footer />
+    </>
+  );
 }
 
 export default MainLayout;
---
+++ src/components/ui/HeaderText.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+interface headingProps {
+  text: string;
+}
+const HeaderText = ({ text }: headingProps) => {
+  return (
+    <div className="flex justify-center items-center w-full bg-[#0F203E]">
+      <h1 className="text-[100px] text-[#D1FAE5] mt-[120px] font-extrabold tracking-normal absolute">
+        {text}
+      </h1>
+      <h1 className="text-[150px] text-[#D1FAE5] opacity-10 font-extrabold tracking-normal">
+        {text}
+      </h1>
+    </div>
+  );
+};
+
+export default HeaderText;
