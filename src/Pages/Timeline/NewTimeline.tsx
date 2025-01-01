c251c426b3a0eb0f765ad3e3f5a9b95e3d8840ca
himanishpuri <himanishpuri2203@gmail.com>
2025-01-01 19:52:22 +0530

    minor

--- src/Pages/Timeline/NewTimeline.tsx
+++ src/Pages/Timeline/NewTimeline.tsx
@@ -1,9 +1,9 @@
-import { timelineElements } from "./timelineElements";
 import {
 	VerticalTimeline,
 	VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import { timelineElements } from "./timelineElements";
 
 export default function NewTimeline() {
 	return (
@@ -23,7 +23,18 @@ export default function NewTimeline() {
 							{element.title}
 						</h1>
 						<p className="text-[#A0BED3] font-space_grotesx">
-							{element.description}
+							{element.description.split("submit.ccstiet.com")[0]}
+							{element.description.includes("submit.ccstiet.com") && (
+								<a
+									href="https://submit.ccstiet.com"
+									className="text-[#A0BED3] font-space_grotesx underline"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									submit.ccstiet.com
+								</a>
+							)}
+							{element.description.split("submit.ccstiet.com")[1]}
 						</p>
 					</VerticalTimelineElement>
 				))}
